8eacfa61bd0151c2beeeba4bc23fd00eb343b1b3
aswin-1016 <aswint1905@gmail.com>
2025-03-10 17:33:14 +0530

    Delete src/main/java/com/ecom/customerOrder/repository directory

--- src/main/java/com/ecom/customerOrder/repository/CustomerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ecom.customerorder.repository;
-
-import com.ecom.customerorder.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> { }
